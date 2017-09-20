7c183f4e20f133882dc7127370688ce72d9f92ef
Chris Nunez <cenunez47@gmail.com>
2017-09-20 18:28:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(x)
+  x.sort
+end
 
+def sort_array_desc(x)
+  x.sort { |y, z| z <=> y }
+end
+
+def sort_array_char_count(x)
+  x.sort { |y, z| y.length <=> z.length }
+end
+
+def swap_elements(x)
+  x[0], x[1], x[2] = x[0], x[2], x[1]
+end
+
+def reverse_array(x)
+  x.reverse
+end
+
+def kesha_maker(x)
+  z = []
+  x.each do |y|
+    z << y.gsub(y[2], "$")
+  end
+  z
+end
+
+def find_a(x)
+  x.find_all {|y| y[0] == "a"}
+end
+
+def sum_array(x)
+end
+
+def add_s(x)
+end
